3f782eaab17f4a919f83263a7feb87f8cb0ce00e
string-aarzoo <varshneyji22@gmail.com>
2019-09-12 14:14:22 +0530

    first commit

---
+++ MeetingCalendar/MeetingCalendar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MeetingCalendar
+//
+//  Created by Aarzoo Varshney on 12/09/19.
+//  Copyright © 2019 Aarzoo Varshney. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
